8acb51d805aecf063e76d419c82ab0c45e63cfe2
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2021-04-10 15:05:36 -0300

    Initial commit

---
+++ trabalho01/c/external_i.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define n 33000
+
+int main(int argc, char *argv[])
+{
+  // Allocate resources
+  struct timespec start, end;
+  double **A = (double **)malloc(n * sizeof(double *));
+  for (unsigned i = 0; i < n; i++)
+    A[i] = (double *)malloc(n * sizeof(double));
+  double *x = (double *)malloc(n * sizeof(double));
+  double *b = (double *)malloc(n * sizeof(double));
+
+  // Initialize A and x with random values between 0 and 1
+  for (long long i = 0; i < n; i++)
+  {
+    x[i] = (double)(rand()) / RAND_MAX;
+    for (unsigned j = 0; j < n; j++)
+      A[i][j] = (double)(rand()) / RAND_MAX;
+  }
+
+  // Compute b = Ax
+  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+  for (long long i = 0; i < n; i++)
+  {
+    double s = 0;
+    for (long long j = 0; j < n; j++)
+      s += A[i][j] * x[j];
+    b[i] = s;
+  }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+  unsigned long delta_ms = (end.tv_sec - start.tv_sec) * 1000 + (end.tv_nsec - start.tv_nsec) / 1000000;
+
+  printf("%lu\n", delta_ms);
+
+  // Return OK
+  return 0;
+}
---
+++ trabalho01/fortran/external_j.f95
@@ -0,0 +1,35 @@
+program main
+
+  integer, parameter :: n=33000
+
+  ! Allocate resources
+  real :: s, rTime, endTime
+  real, dimension(n, n) :: A
+  real, dimension(n) :: x, b
+  integer :: values(8)
+
+  ! Initialize A and x with random values
+  call random_number(A)
+  call random_number(x)
+
+  ! Compute b = Ax
+  call date_and_time(values=values)
+  rTime = (values(5)) * 60.         ! Hours to minutes
+  rTime = (rTime + values(6)) * 60. ! Minutes to seconds
+  rTime = (rTime + values(7)) * 1e3 ! Seconds to milliseconds
+  rTime = rTime + values(8)         ! Add milliseconds
+  do i = 1, n
+    s = 0
+    do j = 1, n
+      s = s + A(j,i) * x(j)
+    end do
+    b(i) = s
+  end do
+  call date_and_time(values=values)
+  endTime = (values(5)) * 60.         ! Hours to minutes
+  endTime = (endTime + values(6)) * 60. ! Minutes to seconds
+  endTime = (endTime + values(7)) * 1e3 ! Seconds to milliseconds
+  endTime = endTime + values(8)         ! Add milliseconds
+  print *, endTime - rTime
+
+end program
